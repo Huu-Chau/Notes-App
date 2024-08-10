ec3bc1a828e2af96505bcf6e01dd4d77e6c2cc4d
unknown <nguyenhuuchau10@gmail.com>
2024-08-10 12:18:25 +0700

    update login use cases

--- backend/routes/login.route.js
+++ backend/routes/login.route.js
@@ -15,39 +15,48 @@ const jwt = require('jsonwebtoken')
 
 // login acc
 loginRouter.post('', async (req, res) => {
-    const { email, password} = req.body
-    
+    const { email, password } = req.body
+
     // check if user enters data
-    if(!email){
-        return res.status(400).json({error: true, message: 'Email is required!'})
+    if (!email) {
+        return res.status(400).json({ error: true, message: 'Email is required!' })
     }
-    
-    if(!password){
-        return res.status(400).json({error: true, message: 'Password is required!'})
+
+    if (!password) {
+        return res.status(400).json({ error: true, message: 'Password is required!' })
     }
 
     // check if email is existing
     const userInfo = await userModel.findOne({ email: email })
 
-    if(!userInfo){
+    if (!userInfo) {
         return res.json({ error: true, message: 'User not found!' })
     }
 
     // check account valid, yes = data, false = error
-    if(userInfo.email == email && userInfo.password == password){
-        const user = {
-            user: userInfo
+    if (userInfo.email == email) {
+        if (userInfo.password == password) {
+            const user = {
+                user: userInfo
+            }
+
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1440m" })
+
+            return res.json({
+                error: false,
+                message: 'Login Sucessful',
+                accessToken
+            })
+        } else {
+            return res.json({
+                error: true,
+                message: 'Wrong password!'
+            })
         }
-        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "1440m"})
-        return res.json({
-            error: false,
-            message: 'Login Sucessful',
-            accessToken
-        })
     } else {
         return res.status(400).json({
             error: true,
-            message: 'Invalid credentials'
+            message: 'Invalid credentials.'
         })
     }
 })
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -10,16 +10,16 @@ function Login() {
   const [error, setError] = useState(null)
 
   const navigate = useNavigate()
-  
+
   const handleLogin = async (e) => {
     e.preventDefault()
 
-    if(!validateEmail(email)){
+    if (!validateEmail(email)) {
       setError('Please enter a valid email address.')
       return
     }
 
-    if(!password){
+    if (!password) {
       setError('Please enter the password.')
       return
     }
@@ -32,14 +32,17 @@ function Login() {
         email: email,
         password: password
       })
-      
+
       // handle successful login response
-      if(response.data && response.data.accessToken){
+      if (response.data && response.data.accessToken) {
         localStorage.setItem("token", response.data.accessToken)
         navigate('/dashboard')
-      } 
+      } else {
+        setError(response.data.message)
+      }
     } catch (error) {
-      if(error.response && error.response.data && error.response.data.message){
+      console.log(error)
+      if (error.response && error.response.data && error.response.data.message) {
         setError(error.response.data.message)
       } else {
         setError('An unexpected error ocurred. Please try again!')
@@ -52,13 +55,13 @@ function Login() {
         <div className='w-96 border rounded bg-white px-8 py-10'>
           <form onSubmit={handleLogin}>
             <h4 className='text-2xl font-medium mb-7'>Log in</h4>
-            <input 
-              type="text" 
-              placeholder='Email' 
-              className='input-box' 
-              value={email} 
+            <input
+              type="text"
+              placeholder='Email'
+              className='input-box'
+              value={email}
               onChange={e => {
-                if(error){
+                if (error) {
                   setError(null)
                 }
                 setEmail(e.target.value)
@@ -69,7 +72,7 @@ function Login() {
               onChange={e => setPassword(e.target.value)}
             />
 
-            {error && 
+            {error &&
               <p className='text-red-500 text-xs pb-1'>{error}</p>
             }
 
